8a17ef78369ff47f1dee6185a753efbc7edb613d
alexwbt <alexwbtg@gmail.com>
2020-01-05 12:19:13 +0800

    edited profile migration and create user seed

--- server/migrations/20200101174817_create-profile.ts
+++ server/migrations/20200101174817_create-profile.ts
@@ -3,11 +3,11 @@ import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<any> {
     await knex.schema.createTable("profile", table => {
-        table.integer("user_id").unsigned().unique();
+        table.integer("user_id").unsigned().unique().notNullable();
         table.foreign("user_id").references("user.id");
         table.string("email").unique().notNullable();
         table.string("image");
-        table.integer("experience").notNullable();
+        table.integer("experience").unsigned().defaultTo(0);
         table.timestamps(false, true);
     });
 }
--- server/seeds/create-users.ts
+++ server/seeds/create-users.ts
@@ -2,8 +2,14 @@ import * as Knex from "knex";
 
 export async function seed(knex: Knex): Promise<any> {
     await knex.raw(`ALTER SEQUENCE user_id_seq RESTART`);
-    await knex.raw(`DELETE FROM "user"`);
+    await knex.delete().from("user");
 
-    await knex.raw(`INSERT INTO "user" (username, password) VALUES (?, ?)`,
-        ["admin", "$2y$12$htPDSCavDcL8sMFr3zWpie1G08dvCyhVIJijQnJEdd6TKA/vImSQi"]);
+    const defaultUser = [
+        { username: "admin", password: "$2y$12$htPDSCavDcL8sMFr3zWpie1G08dvCyhVIJijQnJEdd6TKA/vImSQi" }
+    ];
+    const defaultProfile = [
+        { user_id: 1, email: "alexwbtg@gmail.com" }
+    ];
+    await knex.insert(defaultUser).into("user");
+    await knex.insert(defaultProfile).into("profile");
 };
