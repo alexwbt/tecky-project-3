25c53cadbdd2574d8137399c9c23efec8f2a01f4
alexwbt <alexwbtg@gmail.com>
2020-01-05 12:41:28 +0800

    update create user seed

--- server/seeds/create-users.ts
+++ server/seeds/create-users.ts
@@ -2,14 +2,19 @@ import * as Knex from "knex";
 
 export async function seed(knex: Knex): Promise<any> {
     await knex.raw(`ALTER SEQUENCE user_id_seq RESTART`);
+    await knex.delete().from("profile");
     await knex.delete().from("user");
 
     const defaultUser = [
-        { username: "admin", password: "$2y$12$htPDSCavDcL8sMFr3zWpie1G08dvCyhVIJijQnJEdd6TKA/vImSQi" }
+        { username: "admin", password: "$2y$12$htPDSCavDcL8sMFr3zWpie1G08dvCyhVIJijQnJEdd6TKA/vImSQi", email: "alexwbtg@gmail.com" }
     ];
-    const defaultProfile = [
-        { user_id: 1, email: "alexwbtg@gmail.com" }
-    ];
-    await knex.insert(defaultUser).into("user");
-    await knex.insert(defaultProfile).into("profile");
+    
+    await knex.insert(defaultUser.map(user => ({
+        username: user.username,
+        password: user.password
+    }))).into("user");
+    await knex.insert(defaultUser.map((user, i) => ({
+        user_id: i + 1,
+        email: user.email
+    }))).into("profile");
 };
