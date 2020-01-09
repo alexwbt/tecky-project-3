9922eed11f864c7b07e277a49333fb65a8447d40
alexwbt <alexwbtg@gmail.com>
2020-01-09 16:02:56 +0800

    update creator

--- app/src/containers/Creator.tsx
+++ app/src/containers/Creator.tsx
@@ -17,8 +17,6 @@ import objSprite from "../sprites/objectSprite.png";
 import { uploadProblem } from "../thunks/problemThunk";
 import { IProblemState } from "../reducers/problemReducer";
 import { Prompt } from "react-router-dom";
-import GameEndModal from "../components/canvas/GameEndModal";
-import { faCompressAlt } from "@fortawesome/free-solid-svg-icons";
 
 
 interface ICreatorProps {
