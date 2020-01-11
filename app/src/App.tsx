baeaa6a0f7b74ba40cc7b86ed9b188499a1c9758
alexwbt <alexwbtg@gmail.com>
2020-01-11 10:41:09 +0800

    format app.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -19,26 +19,26 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App: React.FC = () => {
-  return (
-    <Provider store={store}>
-      <MessageBox />
-      <ToastContainer
-      position="top-center"
-      autoClose={3000}
-      hideProgressBar
-      draggable={false}/>
-      <ConnectedRouter history={history}>
-        <Switch>
-          <Route path="/login" exact={true} component={Login} />
-          <PrivateRoute path="/" exact={true} component={Home} />
-          <PrivateRoute path="/profile/:username" component={Profile} />
-          <PrivateRoute path="/leaderBoard" exact={true} component={LeaderBoard} />
-          <PrivateRoute path="/challenge/solve/:problemId" exact={true} component={Solver} />
-          <PrivateRoute path="/challenge/edit/:problemId" exact={true} component={Creator} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </ConnectedRouter>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <MessageBox />
+            <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar
+                draggable={false} />
+            <ConnectedRouter history={history}>
+                <Switch>
+                    <Route path="/login" exact={true} component={Login} />
+                    <PrivateRoute path="/" exact={true} component={Home} />
+                    <PrivateRoute path="/profile/:username" component={Profile} />
+                    <PrivateRoute path="/leaderBoard" exact={true} component={LeaderBoard} />
+                    <PrivateRoute path="/challenge/solve/:problemId" exact={true} component={Solver} />
+                    <PrivateRoute path="/challenge/edit/:problemId" exact={true} component={Creator} />
+                    <Route component={PageNotFound} />
+                </Switch>
+            </ConnectedRouter>
+        </Provider>
+    );
 };
 export default App;
