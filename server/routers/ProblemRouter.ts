59362b603ff8e6ae6f1fc7350b5356bac913a24f
Patrick Chan <chan.patrick21@gmail.com>
2020-01-13 19:33:33 +0800

    fix: bugs

--- server/routers/ProblemRouter.ts
+++ server/routers/ProblemRouter.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, json } from "express";\
+import { Router, Request, Response, json } from "express";
 import * as multer from 'multer';
 import { catcher } from "../middleware";
 import { isLoggedIn } from "../passport";
