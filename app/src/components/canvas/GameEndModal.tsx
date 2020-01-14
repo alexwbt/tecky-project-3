10cd25fd756344133e2294d4f7e65f40a557066f
alexwbt <alexwbtg@gmail.com>
2020-01-14 11:42:00 +0800

    update end modal

--- app/src/components/canvas/GameEndModal.tsx
+++ app/src/components/canvas/GameEndModal.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Button, Modal } from "react-bootstrap";
 import { IProblemDeduction } from "../../models/Problem";
 import ReactCountUp from "react-countup";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDotCircle, faExclamation } from "@fortawesome/free-solid-svg-icons";
 
 
 interface IGameEndModalProps {
@@ -42,51 +44,54 @@ const GameEndModal: React.FC<IGameEndModalProps> = (props: IGameEndModalProps) =
         score -= missedObjDe;
     }
 
+    let failed = props.failed;
+    if (score < props.score / 2) {
+        failed = true;
+    }
+
     return <>
         <Modal size="lg" show={props.show} onHide={props.handleClose}>
             <Modal.Header closeButton>
-                <Modal.Title><h2>{props.failed ? "Sorry." : "Congratulation!"}</h2></Modal.Title>
+                <Modal.Title><h2>{failed ? "Sorry." : "Congratulation!"}</h2></Modal.Title>
             </Modal.Header>
-            {
-                props.failed ?
-                    <Modal.Body>
-                        You fave failed the challenge.
-                    </Modal.Body>
-                    :
-                    <Modal.Body>
-                        <div className="text-center">
-                            <h4>You have completed the challenge.</h4>
-                            <h6>Your Score:</h6>
-                            <h1 className="pb-3" style={{fontSize: 100}}>
-                                <ReactCountUp end={score} start={props.score} duration={2} />
-                                /{props.score}
-                            </h1>
-                        </div>
 
-                        <div>You've used {props.blockCount} blocks.</div>
-                        {
-                            blockCountEx && <div className="text-danger border-bottom">
-                                You've used more than {props.maxUsedBlocks} blocks.
-                                <span className="float-right">(-{blockCountDe} points)</span>
-                            </div>
-                        }
+            <Modal.Body>
+                <div className="text-center">
+                    <h4>{failed ? "You have failed the challenge." : "You have completed the challenge."}</h4>
+                    <h6>Your Score:</h6>
+                    <h1 className={failed ? "text-danger" : "text-success"} style={{ fontSize: 100, paddingBottom: "40px" }}>
+                        <ReactCountUp end={score} start={props.score} duration={2} />
+                        /{props.score}
+                    </h1>
+                </div>
 
-                        <div>You've moved {props.movedTime} times.</div>
-                        {
-                            movedTimesEx && <div className="text-danger border-bottom">
-                                You've moved more than {props.maxMoveTimes} times.
-                                <span className="float-right">(-{movedTimesDe} points)</span>
-                            </div>
-                        }
+                <div>You've used {props.blockCount} blocks.</div>
+                {
+                    blockCountEx && <div className="text-danger border-bottom">
+                        <FontAwesomeIcon icon={faExclamation} className="mr-2" />
+                        You've used more than {props.maxUsedBlocks} blocks.
+                        <span className="float-right">(-{blockCountDe} points)</span>
+                    </div>
+                }
 
-                        {
-                            missedObjEx && <div className="text-danger border-bottom">
-                                You've missed {props.missedObjects} collectables.
-                                <span className="float-right">(-{missedObjDe} points)</span>
-                            </div>
-                        }
-                    </Modal.Body>
-            }
+                <div>You've moved {props.movedTime} times.</div>
+                {
+                    movedTimesEx && <div className="text-danger border-bottom">
+                        <FontAwesomeIcon icon={faExclamation} className="mr-2" />
+                        You've moved more than {props.maxMoveTimes} times.
+                        <span className="float-right">(-{movedTimesDe} points)</span>
+                    </div>
+                }
+
+                {
+                    missedObjEx && <div className="text-danger border-bottom">
+                        <FontAwesomeIcon icon={faExclamation} className="mr-2" />
+                        You've missed {props.missedObjects} collectables.
+                        <span className="float-right">(-{missedObjDe} points)</span>
+                    </div>
+                }
+            </Modal.Body>
+
             <Modal.Footer>
                 <Button variant="secondary" onClick={props.handleClose}>
                     Close
