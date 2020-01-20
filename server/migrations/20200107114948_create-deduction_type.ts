cf582e08964d47c682ef2b240874a3d7fc9eb163
Patrick Chan <chan.patrick21@gmail.com>
2020-01-20 21:55:13 +0800

    move inital data to migration

--- server/migrations/20200107114948_create-deduction_type.ts
+++ server/migrations/20200107114948_create-deduction_type.ts
@@ -1,5 +1,5 @@
 import * as Knex from "knex";
-
+import Tables from "../tables";
 
 export async function up(knex: Knex): Promise<any> {
     await knex.schema.createTable("deduction_type", table => {
@@ -7,6 +7,12 @@ export async function up(knex: Knex): Promise<any> {
         table.string("title").unique().notNullable();
         table.timestamps(false, true);
     });
+
+    await knex(Tables.DEDUCTION_TYPE).insert([
+        { title: "Each blocks more the Max. Used Blocks will lose" },
+        { title: "Move Times more than Max. Move will lose" },
+        { title: "Each object haven't got will lose" }
+    ]);
 }
 
 
--- server/migrations/20200120163557_add-status.ts
+++ server/migrations/20200120163557_add-status.ts
@@ -2,11 +2,12 @@ import * as Knex from "knex";
 import Tables from "../tables";
 
 export async function up(knex: Knex): Promise<any> {
-    await knex(Tables.PROBLEM_STATUS).insert([
-        { name: "Deleted" }
-      ])
+  await knex(Tables.PROBLEM_STATUS).insert([
+    { id: 5, name: "Deleted" }
+  ])
 }
 
 export async function down(knex: Knex): Promise<any> {
+  await knex(Tables.PROBLEM_STATUS).where({ id: 5 }).del();
 }
 
