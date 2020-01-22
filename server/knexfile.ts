ef2c29c141d7e58a3d58650c65348af11c4bc73c
Patrick Chan <chan.patrick21@gmail.com>
2020-01-22 16:34:34 +0800

    add testing in knex file

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -19,6 +19,22 @@ module.exports = {
     }
   },
 
+  testing: {
+    client: "postgresql",
+    connection: {
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
   staging: {
     client: "postgresql",
     connection: {
